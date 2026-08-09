9fd8ec0478ad505f729c4e008f3cdf76e1479333
lucasmarti-igni <lucasmarti1974@gmail.com>
2026-08-09 22:45:25 +0200

    Add proximamente.ts with ItemProximamente type

---
+++ lib/proximamente.ts
@@ -0,0 +1,13 @@
+export type ItemProximamente = {
+  nombre: string;
+  detalle: string;
+};
+
+export const proximamente: ItemProximamente[] = [
+  {
+    nombre: "Maestro Veggie a la Brasa",
+    detalle: "Segundo curso confirmado",
+  },
+  { nombre: "Pastelería", detalle: "Próximamente" },
+  { nombre: "Cocina Regional", detalle: "Próximamente" },
+];
